44334e5d5991b2b931e3d98675ee7f51e34b7480
bartala <bartala@post.bgu.ac.il>
2021-11-15 15:55:21 +0200

    Update 0_main.sh

--- 0_main.sh
+++ 0_main.sh
@@ -14,6 +14,25 @@ mkdir ./bert/data
 mkdir ./bert/bert_output
 echo "finished downloading files for bert"
 
-# fine-tune bert
+# data preperation for fine-tuning bert
 python3 /home/amazonebert.py
 echo "finished extrecting tools"
+
+# fine tune the BERT_base_cased model
+!python3 ./bert/run_classifier.py \
+    --task_name=cola \
+    --do_train=true \
+    --do_eval=true \
+    --do_predict=true \
+    --do_lower_case=False \
+    --data_dir=./bert/data \
+    --vocab_file=./cased_L-12_H-768_A-12/vocab.txt \
+    --bert_config_file=./cased_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=./cased_L-12_H-768_A-12/bert_model.ckpt \
+    --max_seq_length=128 \
+    --learning_rate=3e-5 \
+    --train_batch_size=32 \
+    --num_train_epochs=4.0 \
+    --output_dir=./bert_output/
+    
+ python3 /home/amazonebert_results.py
